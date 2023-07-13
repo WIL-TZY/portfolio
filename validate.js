bd7cb3c0e7300458c7752d49ba7dad8c8be786b0
Nael <willowitzy@gmail.com>
2023-07-13 03:33:55 -0300

    initial commit

---
+++ validate.js
@@ -0,0 +1,31 @@
+/*
+
+const constraints = {
+    username: {
+      presence: true,
+      length: {
+        minimum: 3,
+        maximum: 20,
+        message: "must be between 3 and 20 characters",
+      },
+    },
+    email: {
+      presence: true,
+      email: true,
+    },
+  };
+  
+  const data = {
+    username: "fulano",
+    email: "fulano@example.com",
+  };
+  
+  const validationErrors = validate(data, constraints);
+  
+  if (validationErrors) {
+    console.log(validationErrors);
+  } else {
+    console.log("Data is valid!");
+  }
+
+*/
